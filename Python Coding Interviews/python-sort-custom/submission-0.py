c16315d87edc40f71824b43e901f3eba1f7252f6
Pratham Mehta <pm3483@nyu.edu>
2026-07-07 01:25:14 -0400

    Add: python-sort-custom - submission-0

---
+++ Python Coding Interviews/python-sort-custom/submission-0.py
@@ -0,0 +1,19 @@
+from typing import List
+
+def get_word_length(words):
+    return len(words)
+
+def sort_words(words: List[str]) -> List[str]:
+    return words.sort(key=get_word_length)
+
+def absoule_value(num):
+    return abs(num)
+
+def sort_numbers(numbers: List[int]) -> List[int]:
+    return numbers.sort(key=absoule_value)
+
+
+# do not modify below this line
+print(sort_words(["cherry", "apple", "blueberry", "banana", "watermelon", "zucchini", "kiwi", "pear"]))
+
+print(sort_numbers([1, -5, -3, 2, 4, 11, -19, 9, -2, 5, -6, 7, -4, 2, 6]))
